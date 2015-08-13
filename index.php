7c7c0acf6fa0bc1d6ff48cbb9f13846d28ee9dd4
lindseyemaddox <lindseyemaddox@gmail.com>
2015-08-12 22:20:20 -0500

    finish index page, sans footer and slidey

--- index.php
+++ index.php
@@ -56,19 +56,19 @@
 	  	    		</li>
 
 	            	<li>
-	  	    	    	<span class="icon-graphics"></span>
+	  	    	    	<span class="icon-branding"></span>
 	  	    	    	<h3>Graphics</h3>
 	  	    	    	<p>Need short description here of web design service.</p>
 	  	    		</li>
 
 	            	<li>
-	  	    	    	<span class="icon-branding"></span>
+	  	    	    	<span class="icon-dart"></span>
 	  	    	    	<h3>Branding</h3>
 	  	    	    	<p>Need short description here of web design service.</p>
 	  	    		</li>
 
 	            	<li>
-	  	    	    	<span class="icon-convo"></span>
+	  	    	    	<span class="icon-pen"></span>
 	  	    	    	<h3>Content</h3>
 	  	    	    	<p>Need short description here of web design service.</p>
 	  	    		</li>
@@ -101,13 +101,13 @@
 
 		<div id="people">
 			
-			<article class="container">
+			<article class="container lindsey">
 				
 				<img src="/_assets/img/team-lindsey-maddox.jpg" alt="Lindsey Maddox headshot" title="Lindsey Maddox">
 
 				<div class="blurb">
 					
-					<h4>“Vinyl, documentaries, and a good ole back yard grill.”</h4>
+					<h4>“My label maker is in a box labeled, 'label maker.'”</h4>
 
 					<div class="btn"><a class="button" href="/team">More Lindsey.</a></div><!--btn-->
 
@@ -115,8 +115,65 @@
 
 			</article><!--container-->
 
+			<article class="container abbey">
+				
+				<img src="/_assets/img/team-abbey-smith.jpg" alt="Abbey Smith headshot" title="Abbey Smith">
+
+				<div class="blurb">
+					
+					<h4>“My label maker is in a box labeled, 'label maker.'”</h4>
+
+					<div class="btn"><a class="button" href="/team">More Abbey.</a></div><!--btn-->
+
+				</div><!--blurb-->
+
+			</article><!--container-->
+
+			<article class="container jay">
+				
+				<img src="/_assets/img/team-jay-arnold.jpg" alt="Jay Arnold headshot" title="Jay Arnold">
+
+				<div class="blurb">
+					
+					<h4>“Vinyl, documentaries, and a good ole back yard grill.”</h4>
+
+					<div class="btn"><a class="button" href="/team">More Jay.</a></div><!--btn-->
+
+				</div><!--blurb-->
+
+			</article><!--container-->
+
 		</div><!--people-->
 
+		<hr/>
+
+		<h5>Evidence</h5>
+
+		<div class="work">
+			
+			<div class="work-item"><a href="http://www.angryeagle.com" target="_blank" rel="nofollow"><img src="/_assets/img/work-angry-eagle.jpg" alt="Angry Eagle Lodge & Outfitters website development" title="Angry Eagle Lodge & Outfitters"></a></div><!--work-item-->
+
+			<div class="work-item"><a href="http://www.dykesfoodservice.com/" target="_blank" rel="nofollow"></a><img src="/_assets/img/work-dykes.jpg" alt="Dykes Foodservice website development" title="Dykes Foodservice"></div><!--work-item-->
+
+			<div class="work-item"><a href="http://www.knockoutfitness.net" target="_blank" rel="nofollow"></a><img src="/_assets/img/work-knockout-fitness.jpg" alt="Knockout Fitness website design" title="Knockout Fitness"></div><!--work-item-->
+
+			<div class="work-item"><a href="http://www.tsumensbasketball.com.php53-14.dfw1-1.websitetestlink.com/" target="_blank" rel="nofollow"></a><img src="/_assets/img/work-tsu.jpg" alt="Tennessee State University website design" title="TSU"></div><!--work-item-->
+
+			<div class="work-item"><a href="http://www.energylogic.com.php53-14.dfw1-1.websitetestlink.com/" target="_blank" rel="nofollow"></a><img src="/_assets/img/work-energy-logic.jpg" alt="Energy Logic website build" title="Energy Logic"></div><!--work-item-->
+
+			<div class="work-item"><a href="http://specialolympicstn.org/" target="_blank" rel="nofollow"></a><img src="/_assets/img/work-special-olympics.jpg" alt="Special Olympics TN website design" title="Special Olympics TN"></div><!--work-item-->
+
+			<div class="work-item"><a href="#" target="_blank" rel="nofollow"></a><img src="/_assets/img/work-martha-obryan.jpg" alt="Martha O'Bryan website design" title="Martha O'Bryan Center"></div><!--work-item-->
+
+			<div class="work-item"><a href="http://www.thegardnerschool.com/" target="_blank" rel="nofollow"></a><img src="/_assets/img/work-gardner-school.jpg" alt="The Gardner School website development" title="The Gardner School"></div><!--work-item-->
+
+			<div class="work-item"><a href="http://www.sitemason.com/" target="_blank" rel="nofollow"></a><img src="/_assets/img/work-sitemason.jpg" alt="Sitemason website design" title="Sitemason"></div><!--work-item-->
+
+
+
+		</div><!--work-->
+
+		<hr/>
 
 	</div><!--inner-->
 
